2437b21f32ed598978fbeb17c633a272393d1c86
arnitdo <arnav.nitin.deo@gmail.com>
2023-02-17 19:38:28 +0530

    Add basic authentication limit

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const helmet_1 = require("helmet");
-const authRoutes_1 = require("./routes/authRoutes");
 dotenv.config();
+const authRoutes_1 = require("./routes/authRoutes");
+const applicationRoutes_1 = require("./routes/applicationRoutes");
 const server = express();
 server.use(express.urlencoded({
     extended: true
@@ -13,7 +14,8 @@ server.use(express.urlencoded({
 server.use(express.json());
 server.use(cors());
 server.use((0, helmet_1.default)());
-server.use('/login', authRoutes_1.authRouter);
+server.use('/auth', authRoutes_1.authRouter);
+server.use('/applications', applicationRoutes_1.applicationRouter);
 server.listen(process.env.PORT, () => {
     console.log("Certifyer Backend is up and running on port", process.env.PORT);
 });
